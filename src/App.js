761a58ce1c3f412698283b1e9599548383f9faed
Kevin Li <kli@twitter.com>
2020-12-27 15:57:04 -0800

    push footer to bottom

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <div>
+      <div className="wrapper">
         <AppHeader />
         <Switch>
           <Route component={About} exact path="/about" strict />
